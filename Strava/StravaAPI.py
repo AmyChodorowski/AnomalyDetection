15b480939aca27ca332e24f904cca962aa00b0a6
Amy <amychods@live.com>
2020-07-02 10:42:08 +0100

    Collect, convert and save runs

--- Strava/StravaAPI.py
+++ Strava/StravaAPI.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import pandas as pd
 
 
 class StravaApi:
@@ -57,15 +58,67 @@ class StravaApi:
 
         return runs
 
-    @staticmethod
-    def convert_runs(runs_lt):
+    def convert_runs(self, runs_lt):
         """
         Convert the runs from list() to DataFrame
         :param runs_lt: Runs in a list of DetailedActivity
         :return: pandas.DataFrame
         """
         runs_df = pd.DataFrame()
+
+        for run in runs_lt:
+
+            run = self.flatten_run(run)
+            runs_df = runs_df.append(run, ignore_index=True)
+
         return runs_df
 
-    def save_runs(self):
-        pass
+    def save_runs(self, runs_dt):
+        """
+        Save the runs to file
+        :param runs_dt: pandas.DataFrame of the runs
+        """
+        # First and last run
+        runs_dt.sort_values('start_date', inplace=True)
+        first, last = runs_dt.start_date.min(), runs_dt.start_date.max()
+        first = first.replace('T', '-')
+        last = last.replace('T', '-')
+        first = first.replace(':', '-')
+        last = last.replace(':', '-')
+        first = first.replace('Z', '')
+        last = last.replace('Z', '')
+
+        # Filepath
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dir_path = dir_path.replace('Strava', 'Data')
+        path = os.path.join(dir_path, f"{self.CLIENT_ID}_{first}_{last}_runs.csv")
+
+        runs_dt.to_csv(path, sep=',', header=True)
+
+    @staticmethod
+    def flatten_run(run):
+        """
+        Make the DetailedActivity model a 1D dict
+        :param run: DetailedActivity - https://developers.strava.com/docs/reference/#api-models-DetailedActivity
+        :return: dict()
+        """
+        # Athlete
+        run['athlete_id'] = run['athlete']['id']
+        run.pop('athlete')
+
+        # Start [lat, long] coordinates
+        run['start_lat'] = run['start_latlng'][0]
+        run['start_lng'] = run['start_latlng'][1]
+        run.pop('start_latlng')
+
+        # End [lat, long] coordinates
+        run['end_lat'] = run['end_latlng'][0]
+        run['end_lng'] = run['end_latlng'][1]
+        run.pop('end_latlng')
+
+        # Map
+        run['map_id'] = run['map']['id']
+        run['map_polyline'] = run['map']['summary_polyline']
+        run.pop('map')
+
+        return run
